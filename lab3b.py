be840edbfa90b3dd9720858b7b5959c030dbe90c
mverde <martinverde.edu@gmail.com>
2016-05-22 22:39:27 -0700

    Added missing inode messages

--- lab3b.py
+++ lab3b.py
@@ -25,31 +25,48 @@ def main():
     blocks_pgroup = 0
     inodes_pgroup = 0
 
+    block_freelist_blocknums = []
+    inode_freelist_blocknums = []
+    block_freelist = []
+    inode_freelist = []
+
     blocks_allocated = []
     allocated_inodes = []
 
     with open('super.csv', 'rb') as sup:
         reader = csv.reader(sup)
         curr_list = list(reader)
-        total_inodes = curr_list[0][1]
-        total_blocks = curr_list[0][2]
-        block_size = curr_list[0][3]
-        blocks_pgroup = curr_list[0][5]
-        inodes_pgroup = curr_list[0][6]
+        total_inodes = int(curr_list[0][1])
+        total_blocks = int(curr_list[0][2])
+        block_size = int(curr_list[0][3])
+        blocks_pgroup = int(curr_list[0][5])
+        inodes_pgroup = int(curr_list[0][6])
     
     with open('group.csv', 'rb') as grup:
         reader = csv.reader(grup)
         curr_list = list(reader)
+        for group in curr_list:
+            block_freelist_blocknums.append(int(group[5]))
+            inode_freelist_blocknums.append(int(group[4]))
 
     with open('bitmap.csv', 'rb') as bit:
         reader = csv.reader(bit)
         curr_list = list(reader)
+        i = 0
+        for entry in curr_list:
+            final_entry = map(int, entry)
+            if final_entry[0] == block_freelist_blocknums[i]:
+                block_freelist.append(final_entry)
+            elif final_entry[0] == inode_freelist_blocknums[i]:
+                inode_freelist.append(final_entry)
+            else:
+                i += 1
 
     with open('inode.csv', 'rb') as inod:
         reader = csv.reader(inod)
         curr_list = list(reader)
         for num in curr_list:
-            allocated_inodes.append(Inode(num[0], num[5], num[11:]))
+            allocated_inodes.append(Inode(int(num[0]), int(num[5]), [int(x, 16) for x in num[11:]]))
 
     with open('directory.csv', 'rb') as direc:
         reader = csv.reader(direc)
@@ -57,14 +74,14 @@ def main():
         found = False
         for inode in curr_list:
             for allocated_inode in allocated_inodes:
-                if(allocated_inode.inodenum == inode[4]):
-                    allocated_inode.refby[inode[0]] = inode[1]
-                    allocated_inode.parentinode = inode[0]
+                if(allocated_inode.inodenum == int(inode[4])):
+                    allocated_inode.refby[int(inode[0])] = int(inode[1])
+                    allocated_inode.parentinode = int(inode[0])
                     found = True
             if not found:
-                unallocated = Inode(inode[4], -1, [])
-                unallocated.parentinode = inode[0]
-                unallocated.refby[inode[0]] = inode[1]
+                unallocated = Inode(int(inode[4]), -1, [])
+                unallocated.parentinode = int(inode[0])
+                unallocated.refby[int(inode[0])] = int(inode[1])
                 allocated_inodes.append(unallocated)
             found = False
                 
@@ -75,7 +92,7 @@ def main():
 
     curr_list = []
 
-    output = open('lab3b_check.txt', 'a+')
+    output = open('lab3b_check.txt', 'w+')
 
     for inode in allocated_inodes:
         if inode.numlinks == -1:
@@ -84,6 +101,8 @@ def main():
                 output.write(' ')
                 output.write('DIRECTORY < {} > ENTRY < {} >'.format(key, inode.refby[key]))
             output.write("\n")
+        elif not inode.refby and inode.inodenum > 10:
+            output.write('MISSING INODE < {} > SHOULD BE IN FREE LIST < {} >\n'.format(inode.inodenum, inode_freelist_blocknums[inode.inodenum / inodes_pgroup]))
 
     output.close()
 
