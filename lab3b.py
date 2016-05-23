ff10a6609ecb0c950bb7c63d15c5a97fb368670b
mverde <martinverde.edu@gmail.com>
2016-05-22 19:38:25 -0700

    Added free list init.

--- lab3b.py
+++ lab3b.py
@@ -4,30 +4,57 @@ import csv
 def main():
     #Open files
 
+    inode_freelist = []
+    block_freelist = []
+    curr_list = []
+    total_inodes = 0
+    total_blocks = 0
+    block_size = 0
+    blocks_pgroup = 0
+    inodes_pgroup = 0
+
     with open('super.csv', 'rb') as sup:
         reader = csv.reader(sup)
-        supercsv = list(reader)
+        curr_list = list(reader)
+        total_inodes = curr_list[0][1]
+        total_blocks = curr_list[0][2]
+        block_size = curr_list[0][3]
+        blocks_pgroup = curr_list[0][5]
+        inodes_pgroup = curr_list[0][6]
     
     with open('group.csv', 'rb') as grup:
         reader = csv.reader(grup)
-        groupcsv = list(reader)
+        curr_list = list(reader)
 
     with open('bitmap.csv', 'rb') as bit:
         reader = csv.reader(bit)
-        bitmapcsv = list(reader)
+        curr_list = list(reader)
+        indicator = 0
+        curr_block = bitmapcsv[0][0]
+        for entry in curr_list:
+            if entry[0] != curr_block:
+                curr_block = entry[0]
+                indicator += 1
+            if indicator % 2 == 0:
+                block_freelist += entry
+            else:
+                inode_freelist += entry
 
     with open('inode.csv', 'rb') as inod:
         reader = csv.reader(inod)
-        inodecsv = list(reader)
+        curr_list = list(reader)
 
     with open('directory.csv', 'rb') as direc:
         reader = csv.reader(direc)
-        directorycsv = list(reader)
+        curr_list = list(reader)
 
     with open('indirect.csv', 'rb') as indir:
         reader = csv.reader(indir)
-        indirectcsv = list(reader)
+        curr_list = list(reader)
 
+    curr_list = []
+
+    output = open('lab3b_check.txt', 'r+b')    
         
 if __name__ == "__main__":
         main()
