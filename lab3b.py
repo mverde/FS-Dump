e828eaa1ca70b98f862a5e2a5b19afb6b99c2ecc
mverde <martinverde.edu@gmail.com>
2016-05-22 21:14:28 -0700

    Added unallocated inode messages

--- lab3b.py
+++ lab3b.py
@@ -26,7 +26,8 @@ def main():
     inodes_pgroup = 0
 
     blocks_allocated = []
-    
+    allocated_inodes = []
+
     with open('super.csv', 'rb') as sup:
         reader = csv.reader(sup)
         curr_list = list(reader)
@@ -47,22 +48,26 @@ def main():
     with open('inode.csv', 'rb') as inod:
         reader = csv.reader(inod)
         curr_list = list(reader)
-        allocated_inodes = []
-        for num in inodecsv:
+        for num in curr_list:
             allocated_inodes.append(Inode(num[0], num[5], num[11:]))
 
     with open('directory.csv', 'rb') as direc:
         reader = csv.reader(direc)
         curr_list = list(reader)
+        found = False
         for inode in curr_list:
             for allocated_inode in allocated_inodes:
                 if(allocated_inode.inodenum == inode[4]):
-                    allocated_inode.refby[inode[4]] = inode[1]
+                    allocated_inode.refby[inode[0]] = inode[1]
                     allocated_inode.parentinode = inode[0]
-
-
-
-
+                    found = True
+            if not found:
+                unallocated = Inode(inode[4], -1, [])
+                unallocated.parentinode = inode[0]
+                unallocated.refby[inode[0]] = inode[1]
+                allocated_inodes.append(unallocated)
+            found = False
+                
 
     with open('indirect.csv', 'rb') as indir:
         reader = csv.reader(indir)
@@ -70,8 +75,17 @@ def main():
 
     curr_list = []
 
-    output = open('lab3b_check.txt', 'r+b')    
+    output = open('lab3b_check.txt', 'a+')
 
-    
+    for inode in allocated_inodes:
+        if inode.numlinks == -1:
+            output.write('UNALLOCATED INODE < {} > REFERENCED BY'.format(inode.inodenum))
+            for key in inode.refby:
+                output.write(' ')
+                output.write('DIRECTORY < {} > ENTRY < {} >'.format(key, inode.refby[key]))
+            output.write("\n")
+
+    output.close()
+
 if __name__ == "__main__":
         main()
